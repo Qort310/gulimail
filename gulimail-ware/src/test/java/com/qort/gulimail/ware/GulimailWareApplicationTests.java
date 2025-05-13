aa99284a45b8e365124ebad1e68d1963008c5798
ly <liu1860471@126.com>
2025-05-13 22:45:00 +0800

    项目初始结构创建

---
+++ gulimail-ware/src/test/java/com/qort/gulimail/ware/GulimailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qort.gulimail.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
